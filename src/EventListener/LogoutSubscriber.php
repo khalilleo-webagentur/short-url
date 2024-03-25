1ab5e891a1e219d5def236037acac21d7c2988f5
ronijan <hu@hu.com>
2024-03-26 00:20:01 +0100

    logout subscriber

---
+++ src/EventListener/LogoutSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $token = $event->getToken();
+
+        $request = $event->getRequest();
+
+        $request->getSession()->clear();
+
+        $response = $event->getResponse();
+
+        $response = new RedirectResponse(
+            $this->urlGenerator->generate('app_home'),
+            RedirectResponse::HTTP_SEE_OTHER
+        );
+        
+        $event->setResponse($response);
+    }
+}
