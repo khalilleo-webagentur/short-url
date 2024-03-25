c3bf8fa067b4219243a926b4c7e39deba762d39e
ronijan <hu@hu.com>
2024-03-26 00:20:14 +0100

    remote trait

---
+++ src/Traits/RemoteTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+trait RemoteTrait
+{
+    private function getRemot(): string
+    {
+        $ipAddress = '';
+
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->isValidIpAddress($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipAddressList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+            foreach ($ipAddressList as $ip) {
+                if ($this->isValidIpAddress($ip)) {
+                    $ipAddress = $ip;
+                    break;
+                }
+            }
+        } else if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->isValidIpAddress($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED'];
+        } else if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->isValidIpAddress($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+            $ipAddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        } else if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->isValidIpAddress($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipAddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } else if (!empty($_SERVER['HTTP_FORWARDED']) && $this->isValidIpAddress($_SERVER['HTTP_FORWARDED'])) {
+            $ipAddress = $_SERVER['HTTP_FORWARDED'];
+        } else if (!empty($_SERVER['REMOTE_ADDR']) && $this->isValidIpAddress($_SERVER['REMOTE_ADDR'])) {
+            $ipAddress = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ipAddress;
+    }
+
+    private function isValidIpAddress(string $ip): bool
+    {
+        return !(
+            filter_var(
+                $ip,
+                FILTER_VALIDATE_IP,
+                FILTER_FLAG_IPV4 |
+                    FILTER_FLAG_IPV6 |
+                    FILTER_FLAG_NO_PRIV_RANGE |
+                    FILTER_FLAG_NO_RES_RANGE
+            ) === false);
+    }
+
+    private function getOS(): string
+    {
+        return PHP_OS;
+    }
+
+    private function getAgent()
+    {
+        $browser_info = get_browser(null, true);
+
+        echo "Browser: " . $browser_info->browser . "\n";
+        echo "Browser Version: " . $browser_info->version . "\n";
+        echo "Platform: " . $browser_info->platform . "\n";
+    }
+}
