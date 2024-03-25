3b9a16e46d56e9267e90d714c2e71649c25a826e
ronijan <hu@hu.com>
2024-03-26 00:06:57 +0100

    admin lte asset and controller

---
+++ src/Controller/Admin/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('admin/dashboard')]
+class IndexController extends AbstractController
+{
+    #[Route('/home', name: 'app_admin_index')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+
+        return $this->render('admin/index.html.twig');
+    }
+}
