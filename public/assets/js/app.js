eb747ef7171b74011cac38d03dfc9c23720ec31d
ronijan <hu@hu.com>
2024-04-11 15:02:40 +0200

    code improvments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,22 +4,28 @@ $(document).ready(function () {
     //     $(".alert").alert('close').fadeOut();
     // }, 6000);
 
-    $('#clipboard').on('click', function (e) {
-        let link = $('#link').text();
-        copyContent(link);
-        swal('', 'Link (' + link + ') is copied to clipboard.', 'success');
-    });
+    if ($('#clipboard').length) {
+        $('#clipboard').on('click', function (e) {
+            let link = $('#link').text();
+            copyContent(link);
+            swal('', 'Link (' + link + ') is copied to clipboard.', 'success');
+        });
+    }
 
-    $('.three-dots').on('click', function () {
-        $('.dataId').val($(this).attr('data-id'));
-        console.log($(this).attr('data-id'));
-    });
+    if ($('.three-dots').length) {
+        $('.three-dots').on('click', function () {
+            $('.dataId').val($(this).attr('data-id'));
+            console.log($(this).attr('data-id'));
+        });
+    }
 
-    $('.cUrlCode').on('click', function () {
-        let code = $(this).attr('data-description');
-        copyContent(code);
-        swal('', 'URL-Code (' + code + ') is copied to clipboard.', 'success');
-    });
+    if ($('.cUrlCode').length) {
+        $('.cUrlCode').on('click', function () {
+            let code = $(this).attr('data-description');
+            copyContent(code);
+            swal('', 'URL-Code (' + code + ') is copied to clipboard.', 'success');
+        });
+    }
 
 });
 
