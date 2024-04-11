7ee7239675e3a5c2d729622b3b76ead614975a04
ronijan <hu@hu.com>
2024-04-11 11:37:08 +0200

    webserver bash

---
+++ webserver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+php bin/console cache:clear
+
+echo '----------------------------------------'
+
+composer cc
+
+echo '----------------------------------------'
+
+pwd 
+
+echo '----------------------------------------'
+
+echo 'http://localhost:8080/login'
+
+echo '----------------------------------------'
+
+php bin/console app:new-admin
+
+echo '----------------------------------------'
+
+php -S localhost:8080 -t public
