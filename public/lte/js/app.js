8a009d6365b1b9eb9f08f6d96d62c29099c411c2
ronijan <hu@hu.com>
2024-04-18 19:30:40 +0200

    dashboard delete link and statistic

--- public/lte/js/app.js
+++ public/lte/js/app.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     setTimeout(function () {
         $(".alert").alert('close').fadeOut();
     }, 6000);
+
+    // Delete button on URLS
+    let linkDataId = $('.linkDataId');
+
+    if (linkDataId.length) {
+        linkDataId.on('click', function (e) {
+            e.preventDefault();
+            $('.delLinkId').val($(this).attr('data-id'));
+        });
+    }
 });
--- src/Controller/Admin/Links/IndexController.php
+++ src/Controller/Admin/Links/IndexController.php
@@ -5,19 +5,28 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Links;
 
 use App\Service\LinkService;
+use App\Service\LinkStatisticService;
+use App\Traits\FormValidationTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('admin/dashboard/rD9zA8lC3kU9tA5f')]
+#[Route('admin/dashboard/rD9zA8lC3kU9tA5f/links')]
 class IndexController extends AbstractController
 {
+    use FormValidationTrait;
+
+    private const ADMIN_LINKS_ROUTE = 'app_admin_links_index';
+
     public function __construct(
-        private readonly LinkService $linkService
+        private readonly LinkService $linkService,
+        private readonly LinkStatisticService $linkStatisticService
     ) {
     }
 
-    #[Route('/links/home', name: 'app_admin_links_index')]
+    #[Route('/home', name: 'app_admin_links_index')]
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
@@ -27,5 +36,28 @@ class IndexController extends AbstractController
         return $this->render('admin/links/index.html.twig', [
             'links' => $links,
         ]);
+    }
+
+    #[Route('/delete', name: 'app_admin_link_delete', methods: 'POST')]
+    public function delete(Request $request): RedirectResponse
+    {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+
+        $id = $this->validateNumber($request->request->get('linkId'));
+
+        if ($link = $this->linkService->getById($id)) {
+
+            $this->linkStatisticService->deleteAllByLink($link);
+
+            $this->linkService->delete($link);
+
+            $this->addFlash('success', 'Link and all accoiciated data (if any) has been deleted.');
+
+            return $this->redirectToRoute(self::ADMIN_LINKS_ROUTE);
+        }
+
+        $this->addFlash('warning', 'ID is undefined.');
+
+        return $this->redirectToRoute(self::ADMIN_LINKS_ROUTE);
     }
 }
