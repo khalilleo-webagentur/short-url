d2f782ae40ad6d380c68a6e198174197b761e6ea
khalilleo <dev@khalilleo.com>
2025-05-09 22:44:44 +0200

    composer upgrade

--- webserver.sh
+++ webserver.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
 php bin/console cache:clear
-
 echo '----------------------------------------'
-
 composer cc && composer upgrade -o
-
 echo '----------------------------------------'
-
-pwd 
-
+pwd
 echo '----------------------------------------'
-
 echo 'http://localhost:8080/login'
-
-# echo '----------------------------------------'
-
-# php bin/console app:new-admin
-
 echo '----------------------------------------'
-
 php -S localhost:8080 -t public
