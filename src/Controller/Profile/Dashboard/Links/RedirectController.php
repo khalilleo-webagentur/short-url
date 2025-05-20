024626837b2ce82a6ac527ecc72680ac3b7a407b
khalilleo <dev@khalilleo.com>
2025-05-20 21:02:47 +0200

    malicious-link seite

--- src/Controller/Profile/Dashboard/Links/RedirectController.php
+++ src/Controller/Profile/Dashboard/Links/RedirectController.php
@@ -6,7 +6,9 @@ namespace App\Controller\Profile\Dashboard\Links;
 
 use App\Service\LinkService;
 use App\Service\LinkStatisticService;
+use App\Service\MaliciousUrlsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,12 +18,13 @@ class RedirectController extends AbstractController
 
     public function __construct(
         private readonly LinkService          $linkService,
-        private readonly LinkStatisticService $linkStatisticService
+        private readonly LinkStatisticService $linkStatisticService,
+        private readonly MaliciousUrlsService $maliciousUrlsService,
     ) {
     }
 
     #[Route('/{token}', name: 'app_redirect_url')]
-    public function goto(?string $token): Response
+    public function goto(?string $token): Response|RedirectResponse
     {
         if (!$token) {
             $this->addFlash('notice', 'URL cannot be found.');
@@ -33,6 +36,12 @@ class RedirectController extends AbstractController
         if (!$link) {
             $this->addFlash('notice', 'URL cannot be found.');
             return $this->redirectToRoute(self::HOME_ROUTE);
+        }
+
+        if ($this->maliciousUrlsService->isMaliciousUrl($link->getUrl())) {
+            return $this->render('static/malicious-link.html.twig', [
+                'link' => $link,
+            ]);
         }
 
         if ($link->isPublic()) {
